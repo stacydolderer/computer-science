30685cd34f63c1542a577c706e1f45d65428dffa
stacydolderer <stacy_dolderer@gfps.k12.mt.us>
2019-09-26 18:14:29 -0600

    add pixel art

---
+++ pixel/main.js
@@ -0,0 +1,19 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+// Simple jQuery event handler
+var penColor = "black"
+
+function setPenColor(pen)
+{
+    penColor = pen;
+}
+
+function setPixelColor(pixel)
+{
+    pixel.style.backgroundColor=penColor;
+}
